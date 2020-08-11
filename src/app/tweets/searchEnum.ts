ed54da5c7aa87cb6055a209481671fd0d66a923d
mart610k <martin-baage@hotmail.com>
2020-08-11 13:39:40 +0200

    Added another way to search

---
+++ src/app/tweets/searchEnum.ts
@@ -0,0 +1,5 @@
+export enum Search{
+    date,
+    text,
+    likes
+}
--- src/app/tweets/tweets.component.ts
+++ src/app/tweets/tweets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tweet } from '../tweet';
 import { TweetsService } from '../tweets.service';
+import { Search } from "./searchEnum";
 
 
 @Component({
@@ -9,24 +10,42 @@ import { TweetsService } from '../tweets.service';
   styleUrls: ['./tweets.component.css']
 })
 export class TweetsComponent implements OnInit {
-  wordTyped : string = "";
-
+  wordTyped: string = "";
+  currentSearch: Search = Search.text;
   twat: Tweet[];
   constructor(tService: TweetsService) {
-       this.twat = tService.tweets;
-       console.log(this.twat.length);
-   }
-   searchbarEvents(event){
-      this.wordTyped = event.target.value;
-   }
-   search(text : string) : Boolean{
-     if(this.wordTyped === "" || this.wordTyped === undefined){
-      return false; 
-    }else{
-      return text.toLowerCase().includes(this.wordTyped.toLowerCase());
+    this.twat = tService.tweets;
+    console.log(this.twat.length);
+  }
+  searchbarEvents(event) {
+    this.wordTyped = event.target.value;
+  }
+  search(tweet: Tweet): Boolean {
+    let toReturn: Boolean = false;
+   
+    if (this.wordTyped === "" || this.wordTyped === undefined) {
+      toReturn = false;
     }
-   }
+    else {
+      let contentInfield : string ="";
+      switch (this.currentSearch) {
 
+        case Search.text:
+          contentInfield = tweet.text.toLowerCase();
+          break;
+        case Search.likes:
+          contentInfield = tweet.likes.toString().toLowerCase();
+         break;
+         case Search.date:
+           contentInfield = tweet.date.toString().toLowerCase();
+           break;
+          default:
+            break;
+      }
+      toReturn = contentInfield.includes(this.wordTyped)
+    }
+    return toReturn;
+  }
   ngOnInit() {
   }
 
